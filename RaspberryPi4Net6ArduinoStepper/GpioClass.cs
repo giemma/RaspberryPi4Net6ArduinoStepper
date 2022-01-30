f429307996b9dc8c97780e0836b36f434c7bf9db
Giuseppe Iemma <g.iemma@bitforfun.com>
2022-01-30 14:34:01 +0100

    FirstCommit

---
+++ RaspberryPi4Net6ArduinoStepper/GpioClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaspberryPi4Net6ArduinoStepper
+{
+    public class GpioClass : IDisposable
+    {
+        int pinShow = 20;
+        int pinHide = 21;
+
+        GpioController gpio;
+        public GpioClass()
+        {
+            gpio = new GpioController();
+
+            gpio.OpenPin(pinShow, PinMode.Output);
+            gpio.OpenPin(pinHide, PinMode.Output);
+
+            gpio.Write(pinShow, PinValue.Low);
+            gpio.Write(pinHide, PinValue.Low);
+        }
+
+        public async Task ShowFlag()
+        {
+            gpio.Write(pinShow, PinValue.High);
+            await Task.Delay(50);
+            gpio.Write(pinShow, PinValue.Low);
+        }
+
+        public async Task HideFlag()
+        {
+            gpio.Write(pinHide, PinValue.High);
+            await Task.Delay(50);
+            gpio.Write(pinHide, PinValue.Low);
+        }
+
+        public void Dispose()
+        {
+            gpio.Write(pinShow, PinValue.Low);
+            gpio.Write(pinHide, PinValue.Low);
+            gpio.Dispose();
+        }
+    }
+}
